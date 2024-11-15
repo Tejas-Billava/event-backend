46996d1526a052819fb4c5b58dbb27ea3f797eb5
Tejas-Billava <tejas.billava5@gmail.com>
2024-11-15 21:42:25 +0530

    added port

--- nodeindex.js
+++ nodeindex.js
@@ -243,7 +243,7 @@ app.post("/api/contact", async (req, res) => {
 //   }
 // });
 
-const PORT = 5000;
+const PORT = process.env.port || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
