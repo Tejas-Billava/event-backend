3fbf7ab7d73b42b3571d87b00010e8eb2f85e2eb
Tejas-Billava <tejas.billava5@gmail.com>
2024-11-15 22:30:39 +0530

    Added build script

--- nodeindex.js
+++ nodeindex.js
@@ -203,7 +203,7 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 console.log(PORT);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
